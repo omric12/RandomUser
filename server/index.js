e169e37dcab661d28e57b2bbc2a70b4dce39dc88
Omri <omric12@gmail.com>
2022-03-14 17:30:24 +0200

    api get 10 users

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const axios = require('axios').default;
 
 app.get('/users', (req, res) => {
-  res.json({ users: ['1', '2', '3'] });
+  axios
+    .get('https://randomuser.me/api/?results=10')
+    .then((item) => res.json(item.data.results));
 });
 
 app.listen(5000, () => {
