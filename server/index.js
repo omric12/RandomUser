a8dfb960ea7e65de288a10963e93e3162c4e975e
Omri <omric12@gmail.com>
2022-03-15 00:23:47 +0200

    Deploy

--- server/index.js
+++ server/index.js
@@ -11,22 +11,22 @@ app.use(cors());
  * process.env.PORT while hosted on Heroku
  * 5000 while hosted locally
  */
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const publicPath = path.join(__dirname, '../client/', 'build');
 app.use(express.static(publicPath));
 
 app.get('/users', (req, res) => {
-    axios
-        .get('https://randomuser.me/api/?results=10')
-        .then((response) => {
-            res.send(response.data.results);
-        })
-        .catch((e) => console.log('ERROR BE: ', e));
+  axios
+    .get('https://randomuser.me/api/?results=10')
+    .then((response) => {
+      res.send(response.data.results);
+    })
+    .catch((e) => console.log('ERROR BE: ', e));
 });
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
